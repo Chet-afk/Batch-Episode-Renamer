1eade744e8a42d0f0bc2e566eacaa717993d5988
Project PGX <ProjectPGX@outlook.com>
2022-04-21 19:11:57 -0600

    Added basic Functionality

--- renamer.py
+++ renamer.py
@@ -1,17 +1,28 @@
 import os
 
+def main():
+    
+    
+    filePath = input("Please enter file path: ")
 
-filePath = input("Please enter file path: ")
+
+    os.chdir(filePath)
+    cwd = os.getcwd()
+    
+    print("\nCurrent Directory: " + cwd + "\n")
+
+    files = os.listdir(".")
+
+    
+    renamed = input("What would you like to rename the files to?: ").strip()
+    fileType = input("What is the file extension? (i.e .mkv, .mp4): ").strip()
+    epNum = 1
+    
+    for each_file in files:
+
+        print(each_file)    # Done in a loop for nicer formatting
+        os.rename(each_file, renamed + " Episode " + str(epNum) + fileType)   # Rename instead of Replace so program stops if it tries to replace itself
+        epNum += 1
 
 
-os.chdir(filePath)
-cwd = os.getcwd()
-print("Current Directory: ", cwd)
-
-files = os.listdir(".")
-
-for each_file in files:
-
-    print(each_file)    # Done in a loop for nicer formatting
-    os.rename(each_file, each_file[:-4] + each_file[-4:])   # Rename instead of Replace so program stops if it tries to replace itself
-
+main()
